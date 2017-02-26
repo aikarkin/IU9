df9f5156229e2dcda6a49e1f322dd5625c1acc74
karkin96 <karkin.ai@mail.ru>
2017-02-26 20:47:01 +0300

    added parsing of params from json file

--- CourseWork/pmols/include/hj_algo.h
+++ CourseWork/pmols/include/hj_algo.h
@@ -15,32 +15,39 @@
 namespace ublas = boost::numeric::ublas;
 
 namespace pmols {
+    // WARNING: don't change order of field, it used in parsing
     struct HJParams {
         // size of box to be packed
-        float box_length;
-        float box_width;
-        float box_height;
-        float expansivity;
+        float box_length; // 0
+        float box_width; // 1
+        float box_height; // 2
+        float expansivity; // 3
 
         // initial step in matched directions
-        float step_x;
-        float step_y;
-        float step_z;
-        float step_alpha;
-        float step_betta;
-        float step_gamma;
+        float step_x; // 4
+        float step_y; // 5
+        float step_z; // 6
+        float step_alpha; // 7
+        float step_betta; // 8
+        float step_gamma; // 9
 
-        float lambda; // Hooke Jeeves pattern search step coefficient
-        float step_coefficient;
+        // Hooke Jeeves pattern search step coefficient
+        float lambda; //10
+        float step_coefficient; // 11
 
         // precision of Hooke Jeeves algorithm
-        float trans_eps;
-        float rot_eps;
+        float trans_eps; // 12
+        float rot_eps; // 13
+        float lj_epsilon; // 14
+        float lj_sigma; // 15
+        float cell_length; // 16
 
         // input/output file paths and output file format
+        std::string substance;
         std::string mol_file;
         std::string out_file;
         std::string out_format;
+        std::string distance_func;
 
         std::string to_string() {
             std::stringstream ss;
@@ -61,7 +68,13 @@ namespace pmols {
                       << "\trot_eps:" << rot_eps << "; \n"
                       << "\tmol_file:" << mol_file.c_str() << "; \n"
                       << "\tout_file:" << out_file.c_str() << "; \n"
-                      << "\tout_format:" << out_format.c_str() << "\n}";
+                      << "\tout_format:" << out_format.c_str() << "; \n"
+                      << "\tsubstance: " << substance.c_str() << "; \n"
+                      << "\tlj_epsilon: " << lj_epsilon << "; \n"
+                      << "\tlj_sigma: " << lj_sigma << "; \n"
+                      << "\tcell_length: " << cell_length << "; \n"
+                      << "\tdistance_func: " << distance_func.c_str()
+                      << "\n}";
             return ss.str();
         }
     };
